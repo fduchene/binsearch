fe685790d5fd7a9d6639a86231b600b8ed5ac362
Fabien Duchene <fabien.duchene@uclouvain.be>
2021-01-19 16:11:41 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# BINSEARCH
+> An out-of-bounds error in a binary search 
+
+This bug is part of a collection of different bugs meant to be anaylzed for academic research. 
+These artifacts and the full analysis and code will be available in a scientific paper later on.
+The source of this bug can be found in CBMC's tutorial: http://www.cprover.org/cprover-manual/cbmc/tutorial/
---
+++ binsearch.c
@@ -0,0 +1,27 @@
+// Code from http://www.cprover.org/cprover-manual/cbmc/tutorial/
+//
+int binsearch(int x)
+{
+  int a[16];
+  signed low = 0, high = 17;
+
+  while(low < high)
+  {
+    signed middle = low + ((high - low) >> 1);
+
+    if(a[middle] < x)
+      high = middle;
+    else if(a[middle] > x)
+      low = middle + 1;
+    else // a[middle]==x
+      return middle;
+  }
+
+  return -1;
+}
+
+int main() {
+	int x;
+	x = binsearch(18);
+	x = binsearch(0);
+}
